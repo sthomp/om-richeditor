8ad0582079b249ee0424ada77204b2ab6d92c483
Scott Thompson <thompsons@gilt.com>
2014-04-08 23:29:34 -0400

    Add node rendering components

--- src/om_richeditor/om_richeditor.cljs
+++ src/om_richeditor/om_richeditor.cljs
@@ -9,12 +9,28 @@
 
 (enable-console-print!)
 
+(def data
+  {:dom [{:type "p" :children [{:type "span" :text "Hello world "}
+                               {:type "em" :text "this is emphasized"}
+                               {:type "span" :text " this is regular "}
+                               {:type "a" :text "this is a link" :href "http://www.google.com"}]}
+         {:type "p" :text "Next Paragraph"}]})
+
+(def data2 {:type "a" :text "Some Terminal Node" :href "http://www.google.com"})
+(def data3 [{:type "a" :text "Some Terminal Node" :href "http://www.google.com"}
+            {:type "span" :text "Hello world "}])
+(def data4 [{:type "a" :text "Some Terminal Node" :href "http://www.google.com"}
+            {:type "span" :text "Hello world "}
+            {:type "p" :children [{:type "span" :text "Child1"}
+                                  {:type "span" :text "Child2"}]}])
+
+
 
 (def app-state (atom {:dom [(dom/p #js {:data-row 0} "Row1")
                             (dom/p #js {:data-row 1} "Row2")
                             (dom/p #js {:data-row 2} "Row3")]
-                      :cursor 0}))
-
+                      :cursor {:focusOffset 0
+                               :anchorOffset 0}}))
 
 
 (defn set-cursor [range]
@@ -23,6 +39,84 @@
     (.addRange selection range)
     nil))
 
+(defn construct-node-with-children [data owner]
+  (reify
+    om/IRenderState
+    (render-state [this state]
+                  (println "node-with-children")
+                  (om/build-all
+                     json-node->om-node
+                     (:dom data)
+                     {:init-state state}))))
+
+(defn json-terminal-node->om-node [json-node]
+  (let [text (:text json-node)]
+    (case (:type json-node)
+      "span" (dom/span nil text)
+      "em" (dom/em nil text)
+      "p" (dom/p nil text)
+      "a" (dom/a #js {:href "http://www.yahoo.com" :rel "nofollow" :target "_blank"} text)
+      (throw (js/Error. (str "Unknown node type: " (:type json-node)))))))
+
+
+(defn construct-terminal-node [data owner]
+  (reify
+    om/IRenderState
+    (render-state [this state]
+                  (println "terminal-node")
+                  (let [om-node (json-terminal-node->om-node data)]
+                    (println om-node))
+                  (dom/span nil ()))))
+
+(defn json-node->om-node [json-node]
+  (if (contains? json-node :children)
+    (construct-node-with-children (:children json-node owner))
+    (construct-terminal-node)))
+
+
+(defn comp-terminal-node [data owner]
+  (reify
+    om/IRenderState
+    (render-state [this state]
+                  (json-terminal-node->om-node data))))
+
+;;                   (apply dom/pre nil
+;;                          (map #(om/build comp-terminal-node %) data))
+
+;; (apply dom/pre nil
+;;                          (map (fn [child]
+;;                            (println child)
+;;                            (if (contains? child :children)
+;;                              (apply dom/p nil
+;;                                     (om/build-all comp-node-with-children (:children child)))
+;;                              ((om/build comp-terminal-node child))) data)))
+(defn comp-node-with-children [data owner]
+  (reify
+    om/IRenderState
+    (render-state [this state]
+                  (apply dom/pre nil
+                         (map (fn [child]
+                           (println child)
+                           (if (contains? child :children)
+                             (do
+                               (println "contains children")
+                               (println (:children child))
+                               (dom/span nil "children")
+                               (apply dom/div nil
+                                      om/build comp-node-with-children (:children child)))
+                             (do
+                               (println "no children")
+                               (om/build comp-terminal-node child)))) data)))))
+
+(defn comp-node [data owner]
+  (reify
+    om/IRenderState
+    (render-state [this state]
+
+                  (dom/p nil "editor"))))
+
+(om/root comp-node-with-children data3
+  {:target (. js/document (getElementById "editor"))})
 
 (defn inner-widget [data owner]
   (reify
@@ -30,7 +124,6 @@
     (render-state [this state]
                   (println "blah")
                   data)))
-(count [1 2 3])
 
 (defn widget [data owner]
   (reify
@@ -63,5 +156,5 @@
                  (.collapse rng)
                  (set-cursor rng)))))
 
-(om/root widget app-state
-  {:target (. js/document (getElementById "editor"))})
+
+
