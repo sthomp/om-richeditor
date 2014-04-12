30e4ecb2d45624b07fef2b31e3283362e782b713
Scott Thompson <thompsons@gilt.com>
2014-04-12 17:28:45 -0400

    Add onKeyDown events

--- src/om_richeditor/om_richeditor.cljs
+++ src/om_richeditor/om_richeditor.cljs
@@ -9,17 +9,18 @@
 
 (enable-console-print!)
 
-(def data
-  {:dom [{:type "p" :children [{:type "span" :text "Hello world "}
-                               {:type "em" :text "this is emphasized"}
-                               {:type "span" :text " this is regular "}
-                               {:type "a" :text "this is a link" :attrs {:href "http://www.google.com" :rel "nofollow" :target "_blank"}}]}
-         {:type "p" :text "Next Paragraph"}]})
+(def data3 [{:type "p" :children [{:type "span" :text "Child1"}
+                                  {:type "span" :text "Child2"}]}])
 
 (def data4 [{:type "a" :text "Some Terminal Node2" :attrs {:href "http://www.google.com" :rel "nofollow" :target "_blank"}}
             {:type "span" :text "Hello world "}
             {:type "p" :children [{:type "span" :text "Child1"}
-                                  {:type "span" :text "Child2"}]}])
+                                  {:type "span" :text "Child2"}
+                                  {:type "p" :children [{:type "span" :text "GrandChild1"}
+                                                        {:type "span" :text "GrandChild2"}
+                                                        {:type "p" :children [{:type "span" :text "GrandChild2"}
+                                                                              {:type "span" :text "GrandChild3"}]}]}]}
+            {:type "p" :text "Another line..."}])
 
 
 
@@ -37,25 +38,31 @@
     (.addRange selection range)
     nil))
 
-(defn json-terminal-node->om-node [json-node]
-  "attrs is an *optional* map of attributes. If none is specified then nil will be used."
-  ;; If attrs is specified then we need to do: #js attrs. otherwise, nil.
-  (let [text (:text json-node)
-        js-attrs (clj->js (:attrs json-node))]
-    (case (:type json-node)
-      "span" (dom/span js-attrs text)
-      "em" (dom/em js-attrs text)
-      "p" (dom/p js-attrs text)
-      "a" (dom/a js-attrs text)
-      (throw (js/Error. (str "Unknown node type: " (:type json-node)))))))
+(defn json-terminal-node->om-node
+  "additional-attrs is an *optional* map of additional attributes."
+  ([json-node]
+     (json-terminal-node->om-node json-node {}))
+  ([json-node additional-attrs]
+     (let [text (:text json-node)
+           attrs (merge (:attrs json-node) additional-attrs)
+           js-attrs (clj->js attrs)]
+       (println js-attrs)
+       (println json-node)
+       (case (:type json-node)
+         "span" (dom/span js-attrs text)
+         "em" (dom/em js-attrs text)
+         "p" (dom/p js-attrs text)
+         "a" (dom/a js-attrs text)
+         (throw (js/Error. (str "Unknown node type: " (:type json-node))))))))
 
-(dom/render-to-str (json-terminal-node->om-node (nth data4 0)))
+;; (dom/render-to-str (json-terminal-node->om-node (nth data4 0) {:data-test "test"}))
 
 (defn comp-terminal-node [data owner]
   (reify
     om/IRenderState
     (render-state [this state]
-                  (json-terminal-node->om-node data))))
+                  (let [node (json-terminal-node->om-node data {:contentEditable true :onKeyDown #(println "blah2")})]
+                    node))))
 
 (defn comp-node [data owner]
   (reify
@@ -63,12 +70,10 @@
     (render-state [this state]
                   (if (contains? data :children)
                     (do
-                      (println "contains children")
-                      (:type data)
+                      (:type data)   ;; TODO: Need to use the type as the node
                       (apply dom/pre nil
                              (om/build-all comp-node (:children data))))
                     (do
-                      (println "no children")
                       (om/build comp-terminal-node data))))))
 
 
@@ -76,12 +81,13 @@
   (reify
     om/IRenderState
     (render-state [this state]
-                  (apply dom/div #js {:contentEditable true}
+                  (apply dom/div nil
                          (om/build-all comp-node data)))))
 
-(do
-  (println "------------")
-  (dom/render-to-str (om/build comp-richeditor data4)))
 
 (om/root comp-richeditor data4
   {:target (. js/document (getElementById "editor"))})
+
+;; (do
+;;   (println "------------")
+;;   (dom/render-to-str (om/build comp-keytest data4)))
