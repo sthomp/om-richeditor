4e4d75c45d641c480837332bac641e7721c05a9e
Scott Thompson <thompsons@gilt.com>
2014-04-13 19:38:38 -0400

    Add a channel for mouse cursor changes

--- src/om_richeditor/om_richeditor.cljs
+++ src/om_richeditor/om_richeditor.cljs
@@ -9,21 +9,19 @@
 
 (enable-console-print!)
 
-(def data3 [{:type "p" :children [{:type "span" :text "Child1"}
-                                  {:type "span" :text "Child2"}]}])
-
-(def data4 [{:type "a" :text "Some Terminal Node2" :attrs {:href "http://www.google.com" :rel "nofollow" :target "_blank"}}
-            {:type "span" :text "Hello world"}
-            {:type "p" :children [{:type "span" :text "Child1"}
-                                  {:type "span" :text "Child2"}
-                                  {:type "p" :children [{:type "span" :text "GrandChild1"}
-                                                        {:type "span" :text "GrandChild2"}
-                                                        {:type "p" :children [{:type "span" :text "GrandChild2"}
-                                                                              {:type "span" :text "GrandChild3"}]}]}]}
-            {:type "p" :text "Another line..."}])
+(def data4 (atom {:dom [{:type "a" :text "Some Terminal Node2" :attrs {:href "http://www.google.com" :rel "nofollow" :target "_blank"}}
+                        {:type "span" :text "Hello world"}
+                        {:type "p" :children [{:type "span" :text "Child1"}
+                                              {:type "span" :text "Child2"}
+                                              {:type "p" :children [{:type "span" :text "GrandChild1"}
+                                                                    {:type "span" :text "GrandChild2"}
+                                                                    {:type "p" :children [{:type "span" :text "GrandChild2"}
+                                                                                          {:type "span" :text "GrandChild3"}]}]}]}
+                        {:type "p" :text "Another line..."}]
+                  :cursor {:focusOffset 0}}))
 
 
-(defn set-cursor [range]
+(defn set-dom-cursor [range]
   (let [selection (-> js/window .getSelection)]
     (.removeAllRanges selection)
     (.addRange selection range)
@@ -31,10 +29,11 @@
 
 (defn get-dom-cursor []
   (let [selection (-> js/window .getSelection)]
-    (.log js/console (.-focusNode selection))
-    (.log js/console (.-anchorNode selection))
-    (.log js/console (.. selection -anchorNode -parentNode))
-    selection))
+    {:focusNode (.-focusNode selection)
+     :anchorNode (.-anchorNode selection)
+     :focusOffset (.-focusOffset selection)
+     :anchorOffset (.-anchorOffset selection)}))
+
 
 (defn json-terminal-node->om-node
   "additional-attrs is an *optional* map of additional attributes."
@@ -50,6 +49,26 @@
          "p" (dom/p js-attrs text)
          "a" (dom/a js-attrs text)
          (throw (js/Error. (str "Unknown node type: " (:type json-node))))))))
+
+(defn comp-cursor [data owner {:keys [cursor-chan] :as opts}]
+  (reify
+    om/IWillMount
+    (will-mount [this]
+                (go (loop []
+                      (let [cursor-cmd (<! cursor-chan)
+                            cmd-type (:type cursor-cmd)
+                            args (:args cursor-cmd)]
+                        (println "cursor-cmd" cursor-cmd)
+                        (condp = cmd-type
+                         :add-chr (println "add-char")
+                         :backspace (println "backspace")
+                         :del (println "del")
+                         :click (println "click")
+                         (println "Unknown cursor command")))
+                      (recur))))
+    om/IRenderState
+    (render-state [this state]
+                  (dom/div nil "Cursor"))))
 
 
 (defn comp-terminal-node [data owner]
@@ -73,6 +92,7 @@
                   (let [node (json-terminal-node->om-node data {:onClick (fn [e]
                                                                            (let [click-chan (om/get-shared owner :click-chan)
                                                                                  keypress-chan (om/get-state owner :keypress-chan)]
+                                                                             (println "Current cursor: " (get-dom-cursor))
                                                                              (put! click-chan keypress-chan)))})]
                     node))
     om/IDidUpdate
@@ -83,7 +103,7 @@
                       child (.-firstChild node)]
                   (.selectNode rng child)
                   (.collapse rng false)
-                  (set-cursor rng)))))
+                  (set-dom-cursor rng)))))
 
 (defn comp-node [data owner]
   (reify
@@ -98,35 +118,44 @@
                       (om/build comp-terminal-node data))))))
 
 
+
 (defn comp-richeditor [data owner]
   (reify
     om/IInitState
     (init-state [_]
-                {:keypress-chan nil})
+                {:keypress-chan nil   ;; This chan is set from child terminal nodes when they become focused so we can notify them of keypress
+                 :cursor-chan (chan)  ;; This chan is to send notifications to the cursor that something has changed
+                 })
     om/IWillMount
     (will-mount [_]
                 (let [click-chan (om/get-shared owner :click-chan)]
-                  (println "Starting go loop")
                   (go (loop []
-                        (let [keypress-chan (<! click-chan)]
+                        (let [keypress-chan (<! click-chan)
+                              cursor-chan (om/get-state owner :cursor-chan)]
+                          (put! cursor-chan {:type :click
+                                             :args nil})
                           (om/set-state! owner :keypress-chan keypress-chan))
                         (recur)))))
     om/IRenderState
     (render-state [this state]
-                  (apply dom/div #js {:contentEditable true
-                                      :onKeyDown (fn [e]
-                                                   #_(println "keydown"))
-                                      :onKeyPress (fn [e]
-                                                    #_(println "keypress")
-                                                    (when-let [keypress-chan (:keypress-chan (om/get-state owner))]
-                                                      (put! keypress-chan (.. e -which))))}
-                         (om/build-all comp-node data)))))
+                  (dom/div nil
+                           (apply dom/div #js {:contentEditable true
+                                               :onKeyDown (fn [e]
+                                                            #_(println "keydown"))
+                                               :onKeyPress (fn [e]
+                                                             #_(println "keypress")
+                                                             (.preventDefault e)
+                                                             (when-let [keypress-chan (:keypress-chan (om/get-state owner))]
+                                                               (put! keypress-chan (.. e -which))))}
+                                  (om/build-all comp-node (:dom data)))
+                           (om/build comp-cursor (:cursor data) {:opts {:cursor-chan (:cursor-chan state)}})))))
 
 
 (let [click-chan (chan)]
   (om/root comp-richeditor data4
            {:target (. js/document (getElementById "editor"))
-            :shared {:click-chan click-chan}}))
+            :shared {:click-chan click-chan   ;; Notifies the richeditor when clicks happen on a terminal node
+                     }}))
 
 
 ;; (do
